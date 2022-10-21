dafdc03b7cab3d3fb8eaf1bd2a66bbaabc385715
giovi <giovi@Pc-di-Giovi>
2022-10-21 20:17:50 +0200

    Convertiti i test "prima della consegna" per l'utilizzo di JUnit

--- src/main/Controllore.java
+++ src/main/Controllore.java
@@ -1,10 +1,21 @@
 package main;
 
 import java.lang.reflect.Constructor;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 public class Controllore {
 	
-	public boolean esistenzaInterfaceIo(){
+	boolean risultato;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.risultato=false;
+	}
+	
+	@Test
+	public void esistenzaInterfaceIo(){
 		String nomeAtteso = "it.uniroma3.diadia.IO";
 		try {
 			Class<?> classeIo= Class.forName(nomeAtteso);
@@ -12,11 +23,8 @@ public class Controllore {
 				System.out.println("OK! Esiste l'interface IO.");
 				classeIo.getMethod("leggiRiga");
 				classeIo.getMethod("mostraMessaggio", String.class);
-				return true;
-			}else{
-				throw new ClassNotFoundException();
+				risultato=true;
 			}
-			
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! L'interface IO non esiste, o è stata chiamata diversamente oppure è nel package sbagliato. Assicurarsi che IO sia una interface e non una classe.");
 		} catch (NoSuchMethodException e) {
@@ -24,138 +32,137 @@ public class Controllore {
 		} catch (SecurityException e) {
 			e.printStackTrace();
 		}
-		return false;
+		assertTrue(risultato);
 	}
-
-	public boolean esistenzaBuilderDiadia() {
+	
+	@Test
+	public void esistenzaBuilderDiadia() {
 		Class<?> classeDiaDia;
-		boolean risultato=false;
 		try {
 			classeDiaDia = Class.forName("it.uniroma3.diadia.DiaDia");
 			for(Constructor<?> constructor : classeDiaDia.getDeclaredConstructors()) {
 				if(constructor.getParameterCount()==1 && constructor.getParameterTypes()[0]== Class.forName("it.uniroma3.diadia.IO")) {
-					risultato=true;
+					this.risultato=true;
 				}
 			}
-			if(risultato)
+			if(this.risultato)
 				System.out.println("OK! Esiste il costruttore di diadia richiesto.");
 			else
 				System.out.println("Attenzione! Non esiste il costruttore di DiaDia al quale si puo' passare l'IO.");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! Non e' stata individuata nessuna classe DiaDia. Assicurarsi di aver esportato\nil proprio DiaDia in formato jar e di averlo posizionato nella cartella esatta.");
 		}
-		return risultato;
+		assertTrue(this.risultato);
 	}
 	
-	public boolean esisteLabirinto() {
+	@Test
+	public void esisteLabirinto() {
 		String nomeAtteso = "it.uniroma3.diadia.ambienti.Labirinto";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeLab= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Labirinto ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Labirinto non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteStanza() {
+	@Test
+	public void esisteStanza() {
 		String nomeAtteso = "it.uniroma3.diadia.ambienti.Stanza";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeStanza= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Stanza ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Stanza non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteAttrezzo() {
+	@Test
+	public void esisteAttrezzo() {
 		String nomeAtteso = "it.uniroma3.diadia.attrezzi.Attrezzo";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeAttrezzo= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Attrezzo ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Attrezzo non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteGiocatore() {
+	@Test
+	public void esisteGiocatore() {
 		String nomeAtteso = "it.uniroma3.diadia.giocatore.Giocatore";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeGiocatore= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Giocatore ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Giocatore non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteBorsa() {
+	@Test
+	public void esisteBorsa() {
 		String nomeAtteso = "it.uniroma3.diadia.giocatore.Borsa";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeBorsa= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Borsa ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Borsa non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteComando() {
+	@Test
+	public void esisteComando() {
 		String nomeAtteso = "it.uniroma3.diadia.Comando";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeComando= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Comando ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Comando non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean esisteDiaDia() {
+	@Test
+	public void esisteDiaDia() {
 		String nomeAtteso = "it.uniroma3.diadia.DiaDia";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classeDiadia= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste DiaDia ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe DiaDia non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-
-	public boolean esistePartita() {
+	@Test
+	public void esistePartita() {
 		String nomeAtteso = "it.uniroma3.diadia.Partita";
 		try {
-			@SuppressWarnings("unused")
-			Class<?> classePartita= Class.forName(nomeAtteso);
+			Class.forName(nomeAtteso);
 			System.out.println("OK! Esiste Partita ed e' nel package corretto.");
-			return true;
+			this.risultato=true;
 		} catch (ClassNotFoundException e) {
 			System.out.println("Attenzione! la classe Partita non esiste, e' stata chiamata diversamente o e' nel package sbagliato");
-			return false;
 		}
+		assertTrue(this.risultato);
 	}
 
-	public boolean controlliPrimaDellaConsegna() {
-		return this.esisteDiaDia()&&this.esistePartita()&&this.esisteLabirinto()&&
-				this.esisteStanza()&&this.esisteGiocatore()&&this.esisteComando()&&
-				this.esisteBorsa()&&this.esisteAttrezzo()&&this.esistenzaInterfaceIo()&&
-				this.esistenzaBuilderDiadia();
-	}
+//	public boolean controlliPrimaDellaConsegna() {
+//		return this.esisteDiaDia()&&this.esistePartita()&&this.esisteLabirinto()&&
+//				this.esisteStanza()&&this.esisteGiocatore()&&this.esisteComando()&&
+//				this.esisteBorsa()&&this.esisteAttrezzo()&&/*this.esistenzaInterfaceIo()&&*/
+//				this.esistenzaBuilderDiadia();
+//	}
 }
